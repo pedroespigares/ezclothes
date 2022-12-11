40c11ced721b8328bd0f53ca94601407f9b4958d
pedro_espigares <pedritoesp04@gmail.com>
2022-12-11 20:22:56 +0100

    Cambio de estructura en checkout

--- views.js
+++ views.js
@@ -259,22 +259,23 @@ export class Views {
         <div class="container">
         ${this.writeHeader()}
         <section id="checkout--payment">
-            <h1>Checkout</h1>
+            <h1>Checkout - Datos de envío y pago</h1>
             <div class="checkout--container">
                 <div class="checkout--form">
-                <h2>Datos de envío y pago</h2>
                     <form class="checkout--form--container">
                         <div class="shipment">
+                        <h3>Envío</h3>
                             <input id="checkout--name" name="checkout--name" type="text" placeholder="Nombre" required>
                             <input id="checkout--surname" name="checkout--surname" type="text" placeholder="Apellidos" required>
                             <input id="checkout--phone" name="checkout--phone" type="tel" placeholder="Teléfono (123-456-789)" pattern="[0-9]{3}-[0-9]{3}-[0-9]{3}" required>
-                            <input id="checkout--mail" name="checkout--mail" type="email" placeholder="correo@dominio.com">
+                            <input id="checkout--mail" name="checkout--mail" type="email" placeholder="correo@dominio.com" required>
                             <input id="checkout--address" name="checkout--address" type="text" placeholder="Dirección" required>
                             <input id="checkout--city" name="checkout--city" type="text" placeholder="Ciudad" required>
                             <input id="checkout--postal" name="checkout--postal" type="text" placeholder="Código postal" required>
                             <input id="checkout--country" name="checkout--country" type="text" placeholder="País" required>
                         </div>
                         <div class="payment">
+                        <h3>Pago</h3>
                             <input id="payment--titular" name="payment--titular" type="text" placeholder="Titular" required>
                             <input id="payment--number" name="payment--number" type="text" placeholder="Número de tarjeta" maxlength="16" pattern="^[0-9]{16}$" required>
                             <div>
